378b3007634f40067d4bafb8fcd65d4411650d05
ispa070 <59nyd8m94g@privaterelay.appleid.com>
2026-02-19 14:38:42 +0100

    Electronic.java added with content

--- src/Electronic.java
+++ src/Electronic.java
@@ -1,2 +1,18 @@
-public class Electronic {
+public class Electronic extends LoanItem {
+    private String modelName;
+
+    public Electronic(String title, int loanDays, String modelName){
+        super(title, loanDays);
+        this.modelName = modelName;
+    }
+
+    public double calculateFee(){
+        return 100;
+    }
+
+    public String getDescription(){
+        return "Product: "+this.getTitle()+" Model: "+this.modelName;
+    }
+
+
 }
