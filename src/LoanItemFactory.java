7d4000e27d13c339954808622ce1ddacfd62a68e
ispa070 <59nyd8m94g@privaterelay.appleid.com>
2026-02-19 14:58:55 +0100

    Updated if-tree in LoanItemFactory.java with Electronic

--- src/LoanItemFactory.java
+++ src/LoanItemFactory.java
@@ -9,30 +9,42 @@ public class LoanItemFactory {
     public LoanItem create(String type, Scanner scanner){
 
 
-        if(type.equalsIgnoreCase("book")){//Checks for type book
+        if(type.equalsIgnoreCase("book")){ //Checks for type: book
+            System.out.println("Title: ");
+            String title = scanner.nextLine(); //Get title input
+            System.out.print("Loan duration: ");
+            int loanDays = scanner.nextInt(); //Get loan duration input
+            scanner.nextLine();
+            System.out.print("Author: "); //Get author input
+            String author = scanner.nextLine();
+
+            return new Book(title,loanDays,author);
+
+        } else if(type.equalsIgnoreCase("video")){ //Checks for type: video
+            System.out.println("Title: ");
+            String title = scanner.nextLine(); //Get title input
+            System.out.println("Loan duration: ");
+            int loanDays = scanner.nextInt(); //Get loan duration input
+            scanner.nextLine();
+            System.out.println("Duration: "); //Get video duration input
+            int duration = scanner.nextInt();
+            scanner.nextLine();
+
+            return new Video(title, loanDays, duration);
+
+        } else if(type.equalsIgnoreCase("electronic")){ //Checks for type: electronic
             System.out.println("Title: ");
             String title = scanner.nextLine(); //Get title input
             System.out.print("Loan duration: ");
             int loanDays = scanner.nextInt(); //Get loan duration
             scanner.nextLine();
-            System.out.print("Author: ");
-            String author = scanner.nextLine();
+            System.out.print("Model: "); //Get model input
+            String modelName = scanner.nextLine();
 
-            return new Book(title,loanDays,author);
+            return new Electronic(title,loanDays,modelName);
 
-        }else if(type.equalsIgnoreCase("video")){
-            System.out.println("Title: ");
-            String title = scanner.nextLine();
-            System.out.println("Loan duration: ");
-            int loanDays = scanner.nextInt();
-            scanner.nextLine();
-            System.out.println("Duration: ");
-            int duration = scanner.nextInt();
-            scanner.nextLine();
-
-            return new Video(title, loanDays, duration);
-        }else{
-            System.out.println("Invalid input");
+        } else {
+            System.out.println("Invalid input - please try again. †Valid inputs are (book/video/electronic).\nThanks in advance!");
         }
         return null;
     }
